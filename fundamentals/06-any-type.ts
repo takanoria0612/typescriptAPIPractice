319e67140ec6d4ad6c7663a0fdc7ada5d6030e2c
takanoria0612 <takanorihiraki06@gmail.com>
2023-08-24 23:53:10 +0900

    update

--- fundamentals/06-any-type.ts
+++ fundamentals/06-any-type.ts
@@ -1,3 +1,9 @@
 let lessonsCount: any = 5;
 lessonsCount = 'five'; // No error
 
+let wow: any[] = ["hello", 2, 3];
+wow = ["world", 4, 5]; // No error
+
+function printCourse(title, lessonsCount) {
+  return `Course ${title} has ${lessonsCount} lessons.`;
+}// No error
